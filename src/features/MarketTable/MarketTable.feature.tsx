3d8cea6ef9ea52f6a98b0cb1d3162e63c42db82c
Aleksei Chepurko <aleksey.chepurko@gmail.com>
2020-05-05 14:56:05 +0300

    feat: initial MarketTable feature

---
+++ src/features/MarketTable/MarketTable.feature.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { MarketTable } from "./MarketTable.component";
+import { pending, fold } from "@devexperts/remote-data-ts";
+import { ProductService } from "./services/product";
+import { useObservable } from "../../utils/use-observable.hook";
+import { pipe } from "fp-ts/lib/pipeable";
+import { MarketTableData } from "./services/marketTable.models";
+
+const MarketTableFeature: React.FC = () => {
+  const data = useObservable(ProductService.data$, pending);
+  return (
+    <>
+      {pipe(
+        data,
+        fold<any, MarketTableData, React.ReactNode>(
+          () => null,
+          () => "loading",
+          () => "error",
+          (data) => <MarketTable data={data.data} />
+        )
+      )}
+    </>
+  );
+};
+
+export { MarketTableFeature };
